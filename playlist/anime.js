5b591d3f8a2423c176af25fd09be2142a94eb281
Datmar1610 <marling231221@gmail.com>
2023-12-28 04:57:18 +0700

    update

--- playlist/anime.js
+++ playlist/anime.js
@@ -1,3 +1,14 @@
 const playlistAnime = [
-    
+    {
+        name: "Again",
+        artist: "Yui",
+        image: "images/anime/again.jpg",
+        path: "musics/anime/again.mp3",
+    },
+    {
+        name: "Golden time lover",
+        artist: "Sukima Switch",
+        image: "images/anime/goldentimelover.jpg",
+        path: "musics/anime/goldentimelover.mp3",
+    },
 ]
