7c589ab21cf82a35fca987843e5f1be35a511991
Datmar <marling231221@gmail.com>
2023-12-30 01:15:17 +0700

    update

--- playlist/vietnam.js
+++ playlist/vietnam.js
@@ -1,14 +1,32 @@
 const playlistVietnam = [
   {
-    name: "Thay mặt mọi cô gái yêu anh",
-    artist: "Amee",
-    image: "images/vietnam/ThayMoiCoGaiYeuAnh.jpg",
-    path: "musics/vietnam/ThayMoiCoGaiYeuAnh.mp3",
+    name: "Bé ơi từ từ",
+    artist: "Wren Evans",
+    image: "images/vietnam/Beoitutu.jpg",
+    path: "musics/vietnam/Beoitutu.mp3",
   },
   {
-    name: "Ex's Hate Me",
-    artist: "Amee ft Bray",
-    image: "images/vietnam/ExsHateMe.jpeg",
-    path: "musics/vietnam/ExsHateMe.mp3",
+    name: "Những lời hứa bỏ quên",
+    artist: "Vũ",
+    image: "images/vietnam/Nhungloihuaboquen.jpg",
+    path: "musics/vietnam/Nhungloihuaboquen.mp3",
+  },
+  {
+    name: "Rồi em sẽ gặp một chàng trai khác",
+    artist: "Hippohappy",
+    image: "images/vietnam/Roiemsegapmotchangtraikhac.jpg",
+    path: "musics/vietnam/Roiemsegapmotchangtraikhac.mp3",
+  },
+  {
+    name: "Tan ca",
+    artist: "Đạt Ozy & PiaLinh",
+    image: "images/vietnam/Tanca.jpg",
+    path: "musics/vietnam/Tanca.mp3",
+  },
+  {
+    name: "Từng quen",
+    artist: "Wren Evans",
+    image: "images/vietnam/Tanca.jpg",
+    path: "musics/vietnam/Tanca.mp3",
   },
 ];
